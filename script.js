e55c4f45c6f90b544e644eb6dfd8796e4297457a
alireza <khos0034@algonquinlive.com>
2024-06-22 13:17:22 -0400

    second

--- script.js
+++ script.js
@@ -11,11 +11,29 @@ const createBooking = function (flightnum, passnum = 1, price = 199 * passnum) {
     bookings.push(booking);
 }
 
-createBooking("LH123");
-createBooking("LH123", undefined, 1500);
 
+const flight = "AB123";
+const alireza = {
+    name: "Alireza Khoshnami",
+    passport: 123456
+}
+// first making a copy of out object
+const copy = { ...alireza}
 
+const checkin = function (flightnum, passenger) {
+    flightnum = "BC456";
+    passenger.name = "Mr. " + passenger.name;
+    if (passenger.passport === 123456) {
+        console.log("checked in");
+    }
+    else {
+        alert("wrong passport number");
+    }
+}
 
+checkin(flight, copy); 
+console.log(flight);
+console.log(alireza);
 
 
 
