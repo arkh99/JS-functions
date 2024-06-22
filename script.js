4ad52d3c335fe2671ba8411bba489b812b3b0ecf
alireza <khos0034@algonquinlive.com>
2024-06-22 12:43:42 -0400

    First task done

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 'use strict';
 
+const bookings = []
+const createBooking = function (flightnum, passnum = 1, price = 199 * passnum) {
+    const booking = {
+        flightnum,
+        passnum,
+        price
+    } 
+    console.log(booking);
+    bookings.push(booking);
+}
+
+createBooking("LH123");
+createBooking("LH123", undefined, 1500);
+
+
 
 
 
