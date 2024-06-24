702a5bca54b4c9d30bb2885c63aafa9baf7d4720
alireza <khos0034@algonquinlive.com>
2024-06-24 16:42:53 -0400

    function code challenge

--- script.js
+++ script.js
@@ -86,7 +86,7 @@ const higher = function (str, fn) {
 //     bookings: [],
 // }
 
-// // a function for objects 
+// // a function for objects
 // const book = function(flightnum, name) {
 //     // console.log(`${name} booked a seat on ${this.airline} flight: ${this.code}${flightnum}`);
 //     this.bookings.push({
@@ -139,11 +139,84 @@ const higher = function (str, fn) {
 // const addvat = addtax.bind(null ,.23);
 // console.log(addvat(1500));
 
-const addtax = function(rate) {
-    return function (value) {
-        console.log(value * rate / 100 + value);
+// const addtax = function(rate) {
+//     return function (value) {
+//         console.log(value * rate / 100 + value);
+//     }
+// }
+
+// const addvat = addtax(23);
+// addvat(250);
+
+
+/* 
+Let's build a simple poll app!
+A poll has a question, an array of options from which people can choose, and an
+array with the number of replies for each option. This data is stored in the starter
+'poll' object below.
+Your tasks:
+1. Create a method called 'registerNewAnswer' on the 'poll' object. The
+method does 2 things:
+1.1. Display a prompt window for the user to input the number of the
+selected option. The prompt should look like this:
+What is your favourite programming language?
+0: JavaScript
+1: Python
+2: Rust
+3: C++
+(Write option number)
+1.2. Based on the input number, update the 'answers' array property. For
+example, if the option is 3, increase the value at position 3 of the array by
+1. Make sure to check if the input is a number and if the number makes
+sense (e.g. answer 52 wouldn't make sense, right?)
+2. Call this method whenever the user clicks the "Answer poll" button.
+3. Create a method 'displayResults' which displays the poll results. The
+method takes a string as an input (called 'type'), which can be either 'string'
+or 'array'. If type is 'array', simply display the results array as it is, using
+console.log(). This should be the default option. If type is 'string', display a
+string like "Poll results are 13, 2, 4, 1".
+4. Run the 'displayResults' method at the end of each
+'registerNewAnswer' method call.
+5. Bonus: Use the 'displayResults' method to display the 2 arrays in the test
+data. Use both the 'array' and the 'string' option. Do not put the arrays in the poll
+object! So what should the this keyword look like in this situation?
+The Complete JavaScript Course 21
+Test data for bonus:
+§ Data 1: [5, 2, 3]
+§ Data 2: [1, 5, 3, 9, 6, 1]
+Hints: Use many of the tools you learned about in this and the last section 😉
+GOOD LUCK 😀 */
+
+const poll = {
+    question: "What is your favourite programming language?",
+    options: ["0: JavaScript", "1: Python", "2: Rust", "3:C++"],
+    awnsers: [0 , 0, 0 , 0],
+    registerNewAnswer() {
+        console.log("Enter your awnser");
+        for (const x of this.options) {
+            console.log(x);
+        }
+        const option = prompt("Your option?");
+        if (option >= 0 && option <= 3) {
+            this.awnsers[option]++;
+        }
+        else {
+            console.log("please provide a valid option");
+        }
+        this.displayresults();
+    },
+
+    displayresults() {
+        const type = prompt("choose between string or array");
+        if (type === "array") {
+            console.log(`The results are: ${this.awnsers}`);
+        }
+        else if (type === "string") {
+            console.log(`The results are: ${this.awnsers.toString()}`);
+        } else {
+            console.log("please provide a valid type !");
+        }
     }
 }
 
-const addvat = addtax(23);
-addvat(250);
+document.querySelector(".poll").addEventListener("click", poll.registerNewAnswer.bind(poll));
